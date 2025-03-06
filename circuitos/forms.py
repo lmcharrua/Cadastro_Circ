d2a6df7b0e5fdc08df6f1377560148c2a769fbdf
lmcharrua <luis.m.charrua@sapo.pt>
2025-03-06 23:58:57 +0000

    mais correcoes

--- circuitos/forms.py
+++ circuitos/forms.py
@@ -32,28 +32,5 @@ class CreateCircuitoForm(forms.ModelForm):
 class CircuitoForm(forms.ModelForm):
     class Meta:
         model = Circuitos
-        fields = ['N_Circuito', 
-                  'Data_Rate', 
-                  'Data_Inst', 
-                  'Estado_Cct', 
-                  'Entidade_PTR1', 
-                  'Morada_PTR1', 
-                  'Cod_Post_PTR1',
-                  'Interface_PTR1',
-                  'Equip_PTR1',
-                  'Slot_PTR1',
-                  'Trib_PTR1',
-                  'Entidade_PTR2', 
-                  'Morada_PTR2', 
-                  'Cod_Post_PTR2',
-                  'Interface_PTR2',
-                  'Equip_PTR2',
-                  'Slot_PTR2',
-                  'Trib_PTR2',
-                  'User_Cct',
-                  'Propriedade_Cct',
-                  'Outras_Ref']
-        
-
-        widget = { 'Estado_Cct': ChoiceWidget(choices=[('Activo', 'Activo'), ('Desligado', 'Desligado'), ('Definido', 'Definido')])}
+        fields = "__all__"
 
