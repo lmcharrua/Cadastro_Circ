908894c0fdd0c773d9056e5f73fd870b5307b2c0
lmcharrua <luis.m.charrua@sapo.pt>
2025-03-12 18:28:45 +0000

    inserção de circuitos a funcionar

--- circuitos/urls.py
+++ circuitos/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('lista_cct',views.lista_cct, name="lista_cct"),
     path('ver_circuito/<int:pk>', views.ver_circuito, name='ver_circuito'),
     path('editar_circuito/<int:pk>', views.editar_circuito, name='editar_circuito'),
-
+    path('criar_circuito', views.criar_circuito, name='criar_circuito'),
 ]
