09b182eb78a9348704129c21a90ee5d160121bef
lmcharrua <luis.m.charrua@sapo.pt>
2025-03-30 23:16:07 +0100

    criar e editar dados a funcionar modo básico

--- dados/views.py
+++ dados/views.py
@@ -10,22 +10,24 @@ from .forms import ServDadosForm, terminacaoForm, criarservdadosForm
 @login_required(login_url='userlogin')
 def lista_dados(request):
     l_dados = serv_dados.objects.exclude(Service_status='D')
-    context = {'ldados': l_dados}
+    context = {'l_dados': l_dados}
     return render(request, 'dados/lista_dados.html', context=context)
 
 @login_required(login_url='userlogin')
 def editar_dados(request, pk):
     lisid = serv_dados.objects.get(id=pk)
-    term = serv_dados.terminacao_set.all()
+    # term = serv_dados.terminacao_set.all()
 
-    form = ServDadosForm(instance=l)
+    form = ServDadosForm(instance=lisid)
     if request.method == 'POST':
         form = ServDadosForm(request.POST, instance=lisid)
 
         if form.is_valid():
             form.save()
+            print("gravado")
+            return redirect('lista_dados')
+        print(form.errors)
 
-            return redirect(net)   
     context = {'form':form}
     return render(request, 'dados/editar_dados.html', context=context)
 
@@ -33,8 +35,11 @@ def editar_dados(request, pk):
 def criar_dados(request):
     form = criarservdadosForm(request.POST)
     if form.is_valid():
-        form.save()
-        return redirect('editar_dados')
+        d=form.save()
+        print (d.id)
+        return redirect('editar_dados', pk=d.id)
+    else:
+        print(form.errors)
     context = {'form':form}
     return render(request, 'dados/criar_dados.html', context=context)
 
