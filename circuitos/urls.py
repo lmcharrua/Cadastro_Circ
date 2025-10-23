88ef1e40dcdefeffcbb62e649044e72435008cf8
Luis Figueiredo <luis.m.charrua@sapo.pt>
2025-10-23 16:38:38 +0100

    t2

--- circuitos/urls.py
+++ circuitos/urls.py
@@ -2,12 +2,12 @@
 
 from django.urls import path
 from . import views
-from .views import downteste
+#from .views import downteste
 
 urlpatterns = [
     path('lista_cct',views.lista_cct, name="lista_cct"),
     path('ver_circuito/<int:pk>', views.ver_circuito, name='ver_circuito'),
     path('editar_circuito/<int:pk>', views.editar_circuito, name='editar_circuito'),
     path('criar_circuito', views.criar_circuito, name='criar_circuito'),
-    path('download', downteste.as_view(), name='download'),
+    #path('download', downteste.as_view(), name='download'),
 ]
