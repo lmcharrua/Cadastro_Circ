c9d6408c1d9b64fc2175adfc23ca7252d7c96eca
lmcharrua <luis.m.charrua@sapo.pt>
2026-01-28 08:22:45 +0000

    nova tabela de moradas

--- cmain/models.py
+++ cmain/models.py
@@ -11,13 +11,16 @@ class PasswordHistory(models.Model):
 
 class Moradas(models.Model):
     Entidade = models.CharField(max_length=100)
+    Nome_curto = models.CharField(max_length=50, blank=True, null=True)
     Morada = models.CharField(max_length=255)
     CodigoPostal = models.CharField(max_length=20)
     Localidade = models.CharField(max_length=100, blank=True, null=True)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
+    Freguesia = models.CharField(max_length=100, blank=True, null=True)
     Dicofre = models.CharField(max_length=6, blank=True, null=True)
-
 
     def __str__(self):
         return f"{self.Entidade} - {self.Morada}, {self.CodigoPostal} {self.Localidade}"
 
-      
+     
